ac6cc210b937fe1bb50544fdab4b2c0e5ef4d711
Fernando Issao <fernandokassa@yahoo.com.br>
2018-09-28 10:59:39 -0300

    Inclusão do bubble sort

--- Prj_20092018/src/BubbleSort.java
+++ Prj_20092018/src/BubbleSort.java
@@ -9,5 +9,31 @@
  * @author Ruffy
  */
 public class BubbleSort {
-    
+    public static void main (String args[]){
+        int vet [] = new int [8];
+        int aux = 0;
+        for (int i = 0; i < 8; i++) {
+            vet[i] = (int)(Math.random() * 100);
+            System.out.print(vet[i] + " ");
+        }
+        
+        System.out.print("\n");
+        
+        for(int i = 0; i<8; i++){
+            for(int j = 0; j<7; j++){
+                if(vet[j] > vet[j + 1]){
+                    aux = vet[j];
+                    vet[j] = vet[j+1];
+                    vet[j+1] = aux;
+                }
+            }
+            
+        }
+        
+        for (int i = 0; i < 8; i++) {
+            System.out.print(vet[i] + " ");
+        }
+        
+        
+    }
 }
